95a7d3baf02aba1673f16aaac08d349b8c357fd4
Huang JiaCheng <22302010051@m.fudan.edu.cn>
2024-04-29 16:35:30 +0800

    提交了hilo模块

---
+++ myCPU.srcs/sources_1/new/hilo_reg.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/04/28 15:26:56
+// Design Name: 
+// Module Name: hilo_reg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hilo_reg(
+    input   logic   clk,
+    input   logic   rst,
+
+    input   logic   we,
+    input   logic[`RegDataWidth]    hi_i,
+    input   logic[`RegDataWidth]    lo_i,
+
+    output  logic[`RegDataWidth]    hi_o,
+    output  logic[`RegDataWidth]    lo_o
+    );
+
+    logic[`RegDataWidth]    hi;
+    logic[`RegDataWidth]    lo;
+    assign hi_o = hi;
+    assign lo_o = lo;
+    
+    always_ff @( posedge clk ) begin : FunctionPart
+        if (rst == `RstEnable) begin
+            hi <= `ZeroWord;
+            lo <= `ZeroWord;
+        end else if(we == `WriteEnable) begin
+            // 假如复位无效且能写，存储新值并将新写入的值直接作为新值输出
+            hi <= hi_i;
+            lo <= lo_i;
+        end
+    end
+endmodule
